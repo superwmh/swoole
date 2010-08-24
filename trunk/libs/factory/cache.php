46b9b4e212a6b0edcf6493cc10affee5c7e4fe84
mikan.tenny@gmail.com <mikan.tenny@gmail.com@7f3d60e8-6d25-11de-bafe-0531e9a7ae5b>
2010-08-24 02:44:27 +0000

    Fix bug:缓存时间不生效的bug

--- trunk/libs/factory/cache.php
+++ trunk/libs/factory/cache.php
@@ -1,14 +1,2 @@
 <?php
-$cache_server = parse_url(CACHE_URL);
-if(!isset($cache_server['scheme'])) Error::info('Config.php Error','Cache url error!');
-if($cache_server['scheme']=='file')
-{
-	$cache = new FileCache(FILECACHE_DIR.'/'.$cache_server['fragment'].'.fc');
-}
-elseif($cache_server['scheme']=='memcache')
-{
-	$cache = new Memcache;
-	if(!isset($cache_server['port'])) $cache_server['port']=11211;
-	$cache->connect($cache_server['host'],$cache_server['port']);
-}
-?>
+$cache = new Cache(CACHE_URL);
