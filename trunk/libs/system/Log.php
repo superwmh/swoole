5482e3e3b64c3208f235b2b378a03675c71c0a6f
mikan.tenny@gmail.com <mikan.tenny@gmail.com@7f3d60e8-6d25-11de-bafe-0531e9a7ae5b>
2010-12-27 09:48:48 +0000

    1、增加Log类 2、增加Lock加锁类

---
+++ trunk/libs/system/Log.php
@@ -0,0 +1,40 @@
+<?php
+interface ILog
+{
+    function put($type,$msg);
+}
+
+class Log
+{
+    public $backends = array('DBLog','FileLog','PHPLog');
+    public $backend;
+
+    function __construct($params,$backend='PHPLog')
+    {
+        if(!in_array($backend,$this->backends))
+		{
+			Error::info('Log backend Error',"Log backend <b>$backend</b> not no support!");
+		}
+		import('#log.'.$backend);
+		$this->backend = new $backend($params);
+    }
+    function info($msg)
+    {
+    	$this->backend->put('INFO',$msg);
+    }
+
+    function error($msg)
+    {
+    	$this->backend->put('ERROR',$msg);
+    }
+
+    function warn($msg)
+    {
+        $this->backend->put('WARNNING',$msg);
+    }
+
+	function __call($method,$args=array())
+	{
+		return call_user_func_array(array($this->backend,$method),$args);
+	}
+}
