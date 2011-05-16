94857b97667e5a76d1b815ca1e72fd01f540962b
mikan.tenny@gmail.com <mikan.tenny@gmail.com@7f3d60e8-6d25-11de-bafe-0531e9a7ae5b>
2011-05-16 04:39:12 +0000

    1、清理框架代码，删除历史遗留的无效文件 2、重新归类，使程序代码结构更加简洁，优雅

--- trunk/libs/function/cli.php
+++ trunk/libs/function/cli.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * 导入所有controller
+ * @return unknown_type
+ */
 function import_all_controller()
 {
     global $php;
@@ -16,6 +20,10 @@ function import_all_controller()
     }
     $d->close();
 }
+/**
+ * 导入所有model
+ * @return unknown_type
+ */
 function import_all_model()
 {
     global $php;
@@ -32,6 +40,97 @@ function import_all_model()
         $php->env['controllers'][$name] = $path;
     }
     $d->close();
+}
+/**
+ * 创建控制器类的文件
+ * @param $name
+ * @return unknown_type
+ */
+function create_controllerclass($name,$hello=false)
+{
+    $content  = "";
+    $content .= "<?php\n";
+    $content .= "class {$name} extends Controller\n";
+    $content .= "{\n";
+    $content .= "	function __construct(\$swoole)\n";
+    $content .= "	{\n";
+    $content .= "	    parent::__construct(\$swoole);\n";
+    $content .= "	}\n";
+    //添加一个hello view
+    if($hello)
+    {
+        $content .= "	function index(\$swoole)\n";
+        $content .= "	{\n";
+        $content .= "	    echo 'hello world.This page build by <a href=http://www.swoole.com/>swoole</a>!';\n";
+        $content .= "	}\n";
+    }
+    $content .= "}";
+    file_put_contents(WEBPATH.'/apps/controllers/'.$name.'.php',$content);
+}
+/**
+ * 创建模型类的文件
+ * @param $name
+ * @return unknown_type
+ */
+function create_modelclass($name,$table='')
+{
+    $content  = "";
+    $content .= "<?php\n";
+    $content .= "class {$name} extends Model\n";
+    $content .= "{\n";
+    $content .= "	//Here write Database table's name\n";
+    $content .= "	var \$table = '{$table}';\n";
+    $content .= "}";
+    file_put_contents(WEBPATH.'/apps/models/'.$name.'.model.php',$content);
+}
+/**
+ * 创建必需的目录
+ * @return unknown_type
+ */
+function create_require_dir()
+{
+    /**
+     * 建立MVC目录
+     */
+    if(!is_dir(WEBPATH.'/apps')) mkdir(WEBPATH.'/apps',0755);
+    if(!is_dir(WEBPATH.'/apps/controllers')) mkdir(WEBPATH.'/apps/controllers',0755);
+    if(!is_dir(WEBPATH.'/apps/models')) mkdir(WEBPATH.'/apps/models',0755);
+    if(!is_dir(WEBPATH.'/apps/views')) mkdir(WEBPATH.'/apps/views',0755);
+
+    /**
+     * 建立缓存的目录
+     */
+    if(!is_dir(WEBPATH.'/cache')) mkdir(WEBPATH.'/cache',0755);
+    if(!is_dir(WEBPATH.'/cache/pages_c')) mkdir(WEBPATH.'/cache/pages_c',0777);
+    if(!is_dir(WEBPATH.'/cache/templates_c')) mkdir(WEBPATH.'/cache/templates_c',0777);
+    if(!is_dir(WEBPATH.'/cache/filecache')) mkdir(WEBPATH.'/cache/virtualdata',0777);
+
+    /**
+     * Smarty的模板目录
+     */
+    if(!is_dir(WEBPATH.'/templates')) mkdir(WEBPATH.'/templates',0755);
+
+    /**
+     * 建立静态文件的目录
+     */
+    if(!is_dir(WEBPATH.'/static')) mkdir(WEBPATH.'/static',0755);
+    if(!is_dir(WEBPATH.'/static/images')) mkdir(WEBPATH.'/static/images',0755);
+    if(!is_dir(WEBPATH.'/static/css')) mkdir(WEBPATH.'/static/css',0755);
+    if(!is_dir(WEBPATH.'/static/uploads')) mkdir(WEBPATH.'/static/uploads',0755);
+    if(!is_dir(WEBPATH.'/static/js')) mkdir(WEBPATH.'/static/js',0755);
+
+    /**
+     * 建立外部扩展类目录
+     */
+    if(!is_dir(WEBPATH.'/class')) mkdir(WEBPATH.'/class',0755);
+    /**
+     * 建立网站字典目录
+     */
+    if(!is_dir(WEBPATH.'/dict')) mkdir(WEBPATH.'/dict',0755);
+    /**
+     * 建立Swoole插件系统目录
+     */
+    if(!is_dir(WEBPATH.'/swoole_plugin')) mkdir(WEBPATH.'/swoole_plugin',0755);
 }
 /**
  * server模式下的请求处理函数
