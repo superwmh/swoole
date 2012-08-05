a1f445155fc2dbb2619c95583f43da695bc0412f
Mikan.Tenny <mikan.tenny@gmail.com>
2012-08-05 16:06:01 +0000

    1、支持app+controller+view 3层映射 2、所有全局对象采用延迟加载

--- libs/factory/tpl.php
+++ libs/factory/tpl.php
@@ -5,6 +5,18 @@ global $php;
 $tpl->assign_by_ref('php_genv',$php->genv);
 $tpl->assign_by_ref('php',$php->env);
 
-if(defined('TPL_DIR')) $tpl->template_dir = TPL_DIR;
+if(defined('TPL_DIR'))
+{
+	$tpl->template_dir = TPL_DIR;
+}
+elseif(is_dir(Swoole::$app_path.'/templates'))
+{
+	$tpl->template_dir = Swoole::$app_path.'/templates';
+}
+else
+{
+	$tpl->template_dir = WEBPATH."/templates";
+}
+
 if(DEBUG=='on') $tpl->compile_check = true;
 else $tpl->compile_check = false;
--- libs/system/GeneralView.php
+++ libs/system/GeneralView.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * 通用试图类
+ * 通用视图类
  * 产生一个简单的请求控制，解析的结构，一般用于后台管理系统
- * 简单模拟List  delete  modify  add 4项操
+ * 简单模拟List  delete  modify  add 4项操作
  * @author Tianfeng.Han
  * @package SwooleSystem
  * @subpackage MVC
- *
  */
 class GeneralView
 {
@@ -71,6 +70,7 @@ class GeneralView
     {
         $_GET['action'] = Validate::word($_GET['action']);
         if(!empty($_GET['action'])) $this->action = $_GET['action'];
+        $this->swoole->initApp();
         $method = self::$method_prefix.'_'.$this->action;
         if(method_exists($this,$method)) call_user_func(array($this,$method));
         else Error::info('GeneralView Error!',"View <b>{$this->app_name}->{$method}</b> Not Found!");
